2e8df1240fd2f3041a4c88ab44bef8cc6f0f41b7
Mümin Öztürk <85707419+ozturkmumin@users.noreply.github.com>
2023-06-02 08:28:38 +0000

    Added new package

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import "./styles.css";
-
+import "./scss/styles.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="app-body">
+      <div className="container">
+        <div className="App p-3">
+          <div className="row">
+            <div className="col-lg-2">
+              <div className="d-flex flex-column">
+                <div>Logo</div>
+                <div>TRANSACTIONS</div>
+                <div>STATISTICS</div>
+                <div>PRODUCT</div>
+                <div>CATEGORY</div>
+              </div>
+            </div>
+            <div className="col-lg-6"></div>
+            <div className="col-lg-4"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
